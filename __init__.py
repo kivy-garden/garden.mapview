d2eee2b0ea4491524bc0e1871dff7a6d38af9578
Mathieu Virbel <mat@kivy.org>
2014-08-17 22:39:37 +0200

    add more providers, and use a Spinner to show all the possible maps

--- __init__.py
+++ __init__.py
@@ -77,7 +77,7 @@ class Downloader(object):
             return tile, cache_fn
         tile_y = tile.map_source.get_row_count(tile.zoom) - tile.tile_y - 1
         uri = tile.map_source.url.format(z=tile.zoom, x=tile.tile_x, y=tile_y,
-                              s=choice(["a", "b", "c"]))
+                              s=choice(tile.map_source.subdomains))
         #print "Download {}".format(uri)
         data = urlopen(uri, timeout=5).read()
         with open(cache_fn, "wb") as fd:
@@ -118,22 +118,38 @@ class MapSource(object):
     # list of available providers
     # cache_key: (is_overlay, minzoom, maxzoom, url, attribution)
     providers = {
-        "mapnik": (0, 0, 19, "http://tile.openstreetmap.org/{z}/{x}/{y}.png",
-                   ""),
-        "osm-hot-fr": (0, 0, 19, "http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
-                       ""),
+        "osm": (0, 0, 19, "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            ""),
+        "osm-hot": (0, 0, 19, "http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+            ""),
         "osm-de": (0, 0, 18, "http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png",
-                   "Tiles @ OSM DE"),
+            "Tiles @ OSM DE"),
         "osm-fr": (0, 0, 20, "http://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png",
-                   "Tiles @ OSM France"),
+            "Tiles @ OSM France"),
         "cyclemap": (0, 0, 17, "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
-                   "Tiles @ Andy Allan")
+            "Tiles @ Andy Allan"),
+        "openseamap": (0, 0, 19, "http://tiles.openseamap.org/seamark/{z}/{x}/{y}.png",
+            "Map data @ OpenSeaMap contributors"),
+        "thunderforest-cycle": (0, 0, 19, "http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png",
+            "@ OpenCycleMap via OpenStreetMap"),
+        "thunderforest-transport": (0, 0, 19, "http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png",
+            "@ OpenCycleMap via OpenStreetMap"),
+        "thunderforest-landscape": (0, 0, 19, "http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png",
+            "@ OpenCycleMap via OpenStreetMap"),
+        "thunderforest-outdoors": (0, 0, 19, "http://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png",
+            "@ OpenCycleMap via OpenStreetMap"),
+        "mapquest-osm": (0, 0, 19, "http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpeg",
+            "Tiles Courtesy of Mapquest", {"subdomains": "1234", "image_ext": "jpeg"}),
+        "mapquest-aerial": (0, 0, 19, "http://oatile{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpeg",
+            "Tiles Courtesy of Mapquest", {"subdomains": "1234", "image_ext": "jpeg"}),
+        # more to add with
+        # https://github.com/leaflet-extras/leaflet-providers/blob/master/leaflet-providers.js
     }
 
     def __init__(self,
-        url="http://tile.openstreetmap.org/{z}/{x}/{y}.png",
-        cache_key="mapnik", min_zoom=0, max_zoom=19, tile_size=256,
-        image_ext="png", attribution=""):
+        url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        cache_key="osm", min_zoom=0, max_zoom=19, tile_size=256,
+        image_ext="png", attribution="", subdomains="abc"):
         super(MapSource, self).__init__()
         self.url = url
         self.cache_key = cache_key
@@ -142,14 +158,19 @@ class MapSource(object):
         self.tile_size = tile_size
         self.image_ext = image_ext
         self.attribution = attribution
+        self.subdomains = subdomains
         self.cache_fmt = "{cache_key}_{zoom}_{tile_x}_{tile_y}.{image_ext}"
 
     @staticmethod
     def from_provider(key):
-        is_overlay, min_zoom, max_zoom, url, attribution = \
-            MapSource.providers[key]
+        provider = MapSource.providers[key]
+        options = {}
+        is_overlay, min_zoom, max_zoom, url, attribution = provider[:5]
+        if len(provider) > 5:
+            options = provider[5]
         return MapSource(cache_key=key, min_zoom=min_zoom,
-                         max_zoom=max_zoom, url=url, attribution=attribution)
+                         max_zoom=max_zoom, url=url, attribution=attribution,
+                         **options)
 
     def get_x(self, zoom, lon):
         """Get the x position on the map using this map source's projection
@@ -433,10 +454,10 @@ class MapView(Widget):
         if isinstance(source, string_types):
             self.map_source = MapSource.from_provider(source)
         elif isinstance(source, (tuple, list)):
-            cache_key, min_zoom, max_zoom, url, attribution = source
+            cache_key, min_zoom, max_zoom, url, attribution, options = source
             self.map_source = MapSource(url=url, cache_key=cache_key,
                                         min_zoom=min_zoom, max_zoom=max_zoom,
-                                        attribution=attribution)
+                                        attribution=attribution, **options)
         elif isinstance(source, MapSource):
             self.map_source = source
         else:
@@ -451,6 +472,7 @@ if __name__ == "__main__":
     from kivy.lang import Builder
     try:
         root = Builder.load_string("""
+#:import MapSource __main__.MapSource
 
 <Toolbar@BoxLayout>:
     size_hint_y: None
@@ -478,20 +500,10 @@ RelativeLayout:
         Button:
             text: "Move to Sydney, Autralia"
             on_release: mapview.center_on(-33.867, 151.206)
-    Toolbar:
-        top: root.top - dp(48)
-        Button:
-            text: "Mapnik"
-            on_release: mapview.map_source = "mapnik"
-        Button:
-            text: "OSM FR"
-            on_release: mapview.map_source = "osm-fr"
-        Button:
-            text: "OSM DE"
-            on_release: mapview.map_source = "osm-de"
-        Button:
-            text: "OSM Hot"
-            on_release: mapview.map_source = "osm-hot-fr"
+        Spinner:
+            text: "mapnik"
+            values: MapSource.providers.keys()
+            on_text: mapview.map_source = self.text
 
     Toolbar:
         Label:
