207d96a437695eb3ae9297c6fb08a902a09ef638
Mathieu Virbel <mat@kivy.org>
2014-08-18 13:35:41 +0200

    put the downloader instance within the class itself.

--- __init__.py
+++ __init__.py
@@ -56,13 +56,20 @@ Builder.load_string("""
 
 """)
 
-_downloader = None
 
 def clamp(x, minimum, maximum):
     return max(minimum, min(x, maximum))
 
 
 class Downloader(object):
+    _instance = None
+
+    @staticmethod
+    def instance():
+        if Downloader._instance is None:
+            Downloader._instance = Downloader()
+        return Downloader._instance
+
     def __init__(self):
         super(Downloader, self).__init__()
         self.executor = ThreadPoolExecutor(max_workers=MAX_WORKERS)
@@ -71,16 +78,24 @@ class Downloader(object):
         if not exists(CACHE_DIR):
             makedirs(CACHE_DIR)
 
-    def download(self, tile):
+    def download_tile(self, tile):
         future = self.executor.submit(self._load_tile, tile)
         self._futures.append(future)
+
+    def download(self, url, callback, **kwargs):
+        future = self.executor.submit(self._download_url, url, callback, kwargs)
+        self._futures.append(future)
+
+    def _download_url(self, url, callback, kwargs):
+        r = requests.get(url, **kwargs)
+        return callback, (url, r, )
 
     def _load_tile(self, tile):
         if tile.state == "done":
             return
         cache_fn = tile.cache_fn
         if exists(cache_fn):
-            return tile, cache_fn
+            return setattr, (tile, "source", cache_fn)
         tile_y = tile.map_source.get_row_count(tile.zoom) - tile.tile_y - 1
         uri = tile.map_source.url.format(z=tile.zoom, x=tile.tile_x, y=tile_y,
                               s=choice(tile.map_source.subdomains))
@@ -89,7 +104,7 @@ class Downloader(object):
         with open(cache_fn, "wb") as fd:
             fd.write(data)
         #print "Downloaded {} bytes: {}".format(len(data), uri)
-        return tile, cache_fn
+        return setattr, (tile, "source", cache_fn)
 
     def _check_executor(self, dt):
         try:
@@ -103,8 +118,8 @@ class Downloader(object):
                     continue
                 if result is None:
                     continue
-                tile, fn = result
-                tile.source = fn
+                callback, args = result
+                callback(*args)
         except TimeoutError:
             pass
 
@@ -241,10 +256,8 @@ class MapSource(object):
         if exists(tile.cache_fn):
             tile.source = tile.cache_fn
         else:
-            global _downloader
-            if _downloader is None:
-                _downloader = Downloader()
-            _downloader.download(tile)
+            Downloader.instance().download_tile(tile)
+
 
 class MapMarker(Image):
     anchor_x = NumericProperty(0.5)
