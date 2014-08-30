5327f9e27bf05609b5cc9d20190261d010f41fa7
Mathieu Virbel <mat@kivy.org>
2014-08-30 23:40:52 +0200

    change geojson example to allow testing an external url

--- examples/simple_geojson.py
+++ examples/simple_geojson.py
@@ -1,9 +1,15 @@
 from mapview import MapView
 from mapview.geojson import GeoJsonMapLayer
 from kivy.base import runTouchApp
+import sys
+
+if len(sys.argv) > 1:
+    source = sys.argv[1]
+else:
+    source = "https://storage.googleapis.com/maps-devrel/google.json"
 
 view = MapView()
-layer = GeoJsonMapLayer(source="https://storage.googleapis.com/maps-devrel/google.json")
+layer = GeoJsonMapLayer(source=source)
 view.add_layer(layer)
 
 runTouchApp(view)
