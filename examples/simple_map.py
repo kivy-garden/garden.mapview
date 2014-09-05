7df1577789f4a8f9cd0a5c8e15f59ad94e004910
Mathieu Virbel <mat@kivy.org>
2014-09-05 11:55:58 +0200

    update example to be able to use custom url

--- examples/simple_map.py
+++ examples/simple_map.py
@@ -1,4 +1,12 @@
-from mapview import MapView
+import sys
+from mapview import MapView, MapSource
 from kivy.base import runTouchApp
 
-runTouchApp(MapView())
+kwargs = {}
+if len(sys.argv) > 1:
+    import hashlib
+    url = sys.argv[1]
+    key = hashlib.sha224(url).hexdigest()[:10]
+    kwargs["map_source"] = MapSource(url=url, cache_key=key, attribution="")
+
+runTouchApp(MapView(**kwargs))
