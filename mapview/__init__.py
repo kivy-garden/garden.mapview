14ad79d1804c15b18666c9f461122526affa5e76
Mathieu Virbel <mat@kivy.org>
2014-09-08 11:48:28 +0200

    fix mapview if used within kivy.garden

--- mapview/__init__.py
+++ mapview/__init__.py
@@ -18,6 +18,14 @@ MIN_LONGITUDE = -180.
 MAX_LONGITUDE = 180.
 CACHE_DIR = "cache"
 
+try:
+    # fix if used within garden
+    import sys
+    sys.modules['mapview'] = sys.modules['kivy.garden.mapview.mapview']
+    del sys
+except KeyError:
+    pass
+
 from mapview.types import Coordinate, Bbox
 from mapview.source import MapSource
 from mapview.view import MapView, MapMarker, MapLayer, MarkerMapLayer, \
