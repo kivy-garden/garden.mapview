d0593bdd20b15f89dc7123241dc6c7a564b773dc
qq <qq>
2016-12-05 14:12:13 +1000

    cater for mapview marker reposition for GridLayout/BoxLayout

--- mapview/view.py
+++ mapview/view.py
@@ -316,9 +316,9 @@ class MapView(Widget):
         """Returns the bounding box from the bottom/left (lat1, lon1) to
         top/right (lat2, lon2).
         """
-        x1, y1 = self.to_local(0 - margin, 0 - margin)
-        x2, y2 = self.to_local((self.width + margin),
-                               (self.height + margin))
+        x1, y1 = self.to_local(self.pos[0] - margin, self.pos[1] - margin)
+        x2, y2 = self.to_local((self.pos[0] + self.width + margin),
+                               (self.pos[1] + self.height + margin))
         c1 = self.get_latlon_at(x1, y1)
         c2 = self.get_latlon_at(x2, y2)
         return Bbox((c1.lat, c1.lon, c2.lat, c2.lon))
