dfa29dce0eb511f2e06fc9bcde6e5ad418ae7b41
Philipp Auersperg <phil@bluedynamics.com>
2019-06-05 23:34:51 +0200

    changed user agent

--- mapview/downloader.py
+++ mapview/downloader.py
@@ -16,7 +16,7 @@ from mapview import CACHE_DIR
 DEBUG = "MAPVIEW_DEBUG_DOWNLOADER" in environ
 # user agent is needed because since may 2019 OSM gives me a 429 or 403 server error
 # I tried it with a simpler one (just Mozilla/5.0) this also gets rejected
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+USER_AGENT = 'Kivy-garden.mapview'
 
 
 class Downloader(object):
